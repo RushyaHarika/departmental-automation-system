501b8f10832ca55b87ad7024e89037678abaf6a4
Rushya Harika Maddukuri <40599543+RushyaHarika@users.noreply.github.com>
2021-05-23 12:27:26 +0530

    updated FacultyDataComponent

--- src/components/hodComponents/facultyDataComponent/FacultyDataHome.js
+++ src/components/hodComponents/facultyDataComponent/FacultyDataHome.js
@@ -1,19 +1,30 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
 
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 
 import FacultyAdditionModal from './FacultyAdditionModal';
-
-
-
-function FacultyDataHome(props){
-    const [modalShow, setModalShow] = React.useState(false);
+window.list='';
+function FacultyDataComponent(props){
+    const [modalShow, setModalShow] = useState(false);
+    let [facultyList,setFacultyList]=useState(null);
+    const fetchFacultyList=()=>{
+        fetch("/api/faculty").then((res)=>res.json())
+        .then((data)=>{
+           setFacultyList(data);
+           
+        }) 
+        setModalShow(false);
+    }
+    useEffect(() => {
+         fetchFacultyList();   
+    },[])
+    
         return(
             props.display?
             <div className="container-fluid">
             <h5  className="pt-5 pb-5">Faculty Data</h5> 
-
+                
                 <Table responsive>
                     <thead>
                         <tr>
@@ -27,31 +38,20 @@ function FacultyDataHome(props){
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                        {Array.from({ length: 4 }).map((_, index) => (
-                            <td key={index}>cell {index}</td>
-                        ))}
-                            <td><Button variant="primary">Edit</Button>{' '}</td>
-                            <td><Button variant="danger">Remove</Button>{' '}</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                        {Array.from({ length: 4 }).map((_, index) => (
-                            <td key={index}>cell {index}</td>
-                        ))}
-                            <td><Button variant="primary">Edit</Button>{' '}</td>
-                            <td><Button variant="danger">Remove</Button>{' '}</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                        {Array.from({ length: 4 }).map((_, index) => (
-                            <td key={index}>cell {index}</td>
-                        ))}
-                            <td><Button variant="primary">Edit</Button>{' '}</td>
-                            <td><Button variant="danger">Remove</Button>{' '}</td>
-                        </tr>
-                        <tr>
+                        {(facultyList!=null)?
+                            facultyList.map((item,index)=>(
+                                <tr>
+                                    <td>{index+1}</td>
+                                    <td>{item.eid}</td>
+                                    <td>{item.name}</td>
+                                    <td>{item.mobile}</td>
+                                    <td>{item.qualification}</td>
+                                    <td><Button variant="primary">Edit</Button>{' '}</td>
+                                    <td><Button variant="danger">Remove</Button>{' '}</td>
+                                </tr>
+                            )):""
+                        }
+                    <tr>   
                         <td colSpan='6'></td>
                         <td><Button variant="primary" onClick={() => setModalShow(true)}>Add Faculty</Button></td>
                     </tr>
@@ -62,10 +62,10 @@ function FacultyDataHome(props){
 
       <FacultyAdditionModal
         show={modalShow}
-        onHide={() => setModalShow(false)}
+        onHide={()=>fetchFacultyList()}
       />
                 
             </div>:''
         )
 }
-export default FacultyDataHome;
+export default FacultyDataComponent;
