9b81fda388f8bbdc047b840b5ee43a443ba2f381
Rushya Harika Maddukuri <40599543+RushyaHarika@users.noreply.github.com>
2021-05-23 17:23:39 +0530

    Added SubjectAllocationComponent

--- src/components/hodComponents/subjectAllocationComponent/SubjectAdditionModal.js
+++ src/components/hodComponents/subjectAllocationComponent/SubjectAdditionModal.js
@@ -1,7 +1,87 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Modal from 'react-bootstrap/Modal'
 import Button from 'react-bootstrap/Button'
-function FacultyAdditionModal(props) {
+
+
+function SubjectAdditionModal(props) {
+
+  const faculty = props.faculty;
+  const subjects = props.subjects;
+
+  let [courseCodeSelectList,setCourseCodeSelectList]=useState([]);
+  let [facultyIDSelectList,setFacultyIDSelectList]=useState([]);
+  let [facultySelectList,setFacultySelectList]=useState([]);
+  let [subjectSelectList,setSubjectSelectList]=useState([]);
+
+  const fetchCourseCodeSelectList=() => {
+    subjects.map((item,index)=>(
+        setCourseCodeSelectList((courseCodeSelectList) => [...courseCodeSelectList,item.courseCode])        
+    ))
+    console.log(courseCodeSelectList);
+  }
+
+  const fetchFacultyIDSelectList=() => {
+    faculty.map((item,index)=>(
+        setFacultyIDSelectList((facultyIDSelectList) => [...facultyIDSelectList,item.fid])        
+    ))
+    console.log(facultyIDSelectList);
+  }
+
+  const fetchFacultySelectList=() => {
+    faculty.map((item,index)=>(
+        setFacultySelectList((facultySelectList) => [...facultySelectList,item.name])        
+    ))
+    console.log(facultySelectList);
+  }
+
+  const fetchSubjectSelectList=() => {
+      subjects.map((item,index)=>(
+          setSubjectSelectList((subjectSelectList) => [...subjectSelectList,item.courseName])        
+      ))
+      console.log(subjectSelectList); 
+  }
+
+    useEffect(() => {
+      setSubjectSelectList([]);
+      setFacultySelectList([]);
+      setCourseCodeSelectList([]);
+      setFacultyIDSelectList([]);
+      fetchFacultySelectList();
+      fetchSubjectSelectList();
+      fetchCourseCodeSelectList();
+      fetchFacultyIDSelectList();   
+ },[])
+
+    const [facultyID,setFacultyID]=useState('');
+    const [facultyName,setFacultyName]=useState('');
+    const [courseCode,setCourseCode]=useState('');
+    const [courseName,setCourseName]=useState('');
+    
+    const PostSubjectAllocation =async (e)=>{
+      e.preventDefault();
+      const res=await fetch("/api/subjectAllocation",{
+        method:"POST",
+        headers:{
+          "Content-type":"application/json"
+        },
+        body:JSON.stringify({
+          facultyID,facultyName,courseCode,courseName
+        })
+      });
+      const data=await res.json();
+      console.log(data)
+      if(res.status===422 || !data){
+        window.alert("Invalid Registration");
+        console.log("Invalid")
+      }else{
+        window.alert("Successfull");
+        setFacultyID('');
+        setFacultyName('');
+        setCourseCode('');
+        setCourseName('');
+      }
+    }
+
     return (
       <Modal
         {...props}
@@ -17,27 +97,51 @@ function FacultyAdditionModal(props) {
         <Modal.Body>
           <form>
               <div className="row container-fluid p-3">
-                <select className="col-sm-4" name="subjects">
-                    <option value="subject 1">subject 1</option>
-                    <option value="subject 2">subject 2</option>
-                    <option value="subject 3">subject 3</option>
-                    <option value="subject 4">subject 4</option>
+                <select className="col-sm-4" name="subjectsID" value={courseCode} onChange={ e => setCourseCode(e.target.value)}>
+                    <option>Select Course Code</option>
+                    {(courseCodeSelectList!=null)?
+                            courseCodeSelectList.map((item,index)=>(
+                              <option>{item}</option>
+                            )):""
+                    }
                 </select>
                 <div className="col-sm-4"></div>
-                <select className="col-sm-4" name="faculty">
-                    <option value="faculty 1">faculty 1</option>
-                    <option value="faculty 2">faculty 2</option>
-                    <option value="faculty 3">faculty 3</option>
-                    <option value="faculty 4">faculty 4</option>
+                <select className="col-sm-4" name="facultyID" value={facultyID} onChange={ e => setFacultyID(e.target.value)}>
+                  <option>Select Faculty ID</option>
+                    {(facultyIDSelectList!=null)?
+                            facultyIDSelectList.map((item,index)=>(
+                              <option>{item}</option>
+                            )):""
+                    } 
+                </select>
+                </div>
+
+                <div className="row container-fluid p-3">
+                <select className="col-sm-4" name="subjects" value={courseName} onChange={ e => setCourseName(e.target.value)}>
+                    <option>Select Subject</option>
+                    {(subjectSelectList!=null)?
+                            subjectSelectList.map((item,index)=>(
+                              <option>{item}</option>
+                            )):""
+                    }
+                </select>
+                <div className="col-sm-4"></div>
+                <select className="col-sm-4" name="faculty" value={facultyName} onChange={ e => setFacultyName(e.target.value)}>
+                  <option>Select Faculty</option>
+                    {(facultySelectList!=null)?
+                            facultySelectList.map((item,index)=>(
+                              <option>{item}</option>
+                            )):""
+                    } 
                 </select> 
               </div>
                
           </form>
         </Modal.Body>
         <Modal.Footer>
-            <Button variant='primary' type='submit' style={{float:'right'}}>Add</Button>
+            <Button variant='primary' type='submit' style={{float:'right'}} onClick={PostSubjectAllocation}>Add</Button>
         </Modal.Footer>
       </Modal>
     );
   }
-  export default FacultyAdditionModal;
+  export default SubjectAdditionModal;
--- src/components/hodComponents/subjectAllocationComponent/SubjectAllocationHome.js
+++ src/components/hodComponents/subjectAllocationComponent/SubjectAllocationHome.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import SubjectAdditionModal from './SubjectAdditionModal';
 
 function SubjectAllocationComponent(props){
+    window.list='';
     const [modalShow, setModalShow] = React.useState(false);
+    let [facultyList,setFacultyList]=useState(null);
+    let [subjectList,setSubjectList]=useState(null);
+    let [subjectAllocationList,setSubjectAllocationList]=useState(null);
+
+  const fetchFacultyList=()=>{
+      fetch("/api/faculty").then((res)=>res.json())
+      .then((data)=>{
+         setFacultyList(data);
+         console.log("Faculty list"+facultyList);
+      }) 
+      setModalShow(false);
+  }
+
+  const fetchSubjectList=()=>{
+    fetch("/api/subject").then((res)=>res.json())
+    .then((data)=>{
+       setSubjectList(data);
+       console.log("Subject list"+subjectList);
+    }) 
+    setModalShow(false);
+ }
+
+ const fetchSubjectAllocationList=()=>{
+    fetch("/api/subjectAllocation").then((res)=>res.json())
+    .then((data)=>{
+       setSubjectAllocationList(data);
+       console.log("Subject Allocation list"+subjectAllocationList);
+    }) 
+    setModalShow(false);
+ }
+
+  useEffect(() => {
+       fetchFacultyList();
+       fetchSubjectList();
+       fetchSubjectAllocationList();   
+  },[])
+
         return(
             props.display?
             <div className="container-fluid">
@@ -16,48 +54,41 @@ function SubjectAllocationComponent(props){
                         <tr>
                             <th>#</th>
                             <th>Course code</th>
-                            <th>Subject</th>
+                            <th>Course Name</th>
                             <th>Faculty ID</th>
                             <th>Faculty name</th>
-                            <th>Edit details</th>
                             <th>Remove Subject</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>1</td>
-                        {Array.from({ length: 4 }).map((_, index) => (
-                            <td key={index}>cell {index}</td>
-                        ))}
-                            <td><Button variant="primary">Edit</Button>{' '}</td>
-                            <td><Button variant="danger">Remove</Button>{' '}</td>
-                        </tr>
-                        <tr>
-                            <td>2</td>
-                        {Array.from({ length: 4 }).map((_, index) => (
-                            <td key={index}>cell {index}</td>
-                        ))}
-                            <td><Button variant="primary">Edit</Button>{' '}</td>
-                            <td><Button variant="danger">Remove</Button>{' '}</td>
-                        </tr>
-                        <tr>
-                            <td>3</td>
-                        {Array.from({ length: 4 }).map((_, index) => (
-                            <td key={index}>cell {index}</td>
-                        ))}
-                            <td><Button variant="primary">Edit</Button>{' '}</td>
-                            <td><Button variant="danger">Remove</Button>{' '}</td>
-                        </tr>
-                        <tr>
-                        <td colSpan='6'></td>
-                        <td><Button variant="primary" onClick={() => setModalShow(true)}>Add Subject</Button></td>
+                    {(subjectAllocationList!=null)?
+                            subjectAllocationList.map((item,index)=>(
+                                <tr>
+                                    <td>{index+1}</td>
+                                    <td>{item.courseCode}</td>
+                                    <td>{item.courseName}</td>
+                                    <td>{item.facultyID}</td>
+                                    <td>{item.facultyName}</td>
+                                    <td><Button variant="danger">Remove</Button>{' '}</td>
+                                </tr>
+                            )):""
+                        }
+                   <tr>   
+                        <td colSpan='5'></td>
+                        <td><Button variant="primary" onClick={() => setModalShow(true)}>Allocate Subject</Button></td>
                     </tr>
                     </tbody>
                 </Table>
 
                 <SubjectAdditionModal
                     show={modalShow}
-                    onHide={() => setModalShow(false)}
+                    onHide={() => {
+                        fetchFacultyList();
+                        fetchSubjectList();
+                        fetchSubjectAllocationList();
+                    }} 
+                    faculty={facultyList}
+                    subjects={subjectList}
                 />   
             </div>:''
         )
