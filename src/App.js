24faa744872aca0877f193c4b4fab3dec032d13c
Rushya Harika Maddukuri <40599543+RushyaHarika@users.noreply.github.com>
2021-05-30 12:47:46 +0530

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React from "react"
-import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import {BrowserRouter,Router,Switch,Route} from 'react-router-dom';
 import LoginComponent from './components/LoginComponent';
-import HodHomeComponent from './components/HodHomeComponent';
-import FacultyHomeComponent from './components/FacultyHomeComponent';
+import HodHomeComponent from './components/hodComponents/HodHomeComponent';
+import FacultyHomeComponent from './components/facultyComponents/FacultyHomeComponent';
+import history from './history';
 
 function App() {
     return (
+        <BrowserRouter>
         <Router>
             <Switch>
                 <Route path="/" exact component={LoginComponent}/>
                 <Route path="/hod" exact component={HodHomeComponent}/>
-                <Route path="/faculty" exact component={FacultyHomeComponent}/>
+                <Route path="/faculty/:id" exact component={FacultyHomeComponent}/>
             </Switch>   
         </Router>
+        </BrowserRouter>
     )
 }
 
