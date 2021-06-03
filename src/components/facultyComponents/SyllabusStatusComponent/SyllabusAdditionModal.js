469af0f3786da502602f602927edafe0eb593ffb
Jyothsna Devi Muramalla <45603381+Jyothsna99@users.noreply.github.com>
2021-06-03 19:59:20 +0530

    Add files via upload

---
+++ src/components/facultyComponents/SyllabusStatusComponent/SyllabusAdditionModal.js
@@ -0,0 +1,127 @@
+import React,{ useState ,useEffect} from 'react';
+import Modal from 'react-bootstrap/Modal';
+import Button from 'react-bootstrap/Button';
+import {useParams} from "react-router-dom";
+
+function SyllabusAdditionModal(props) {
+    const params=useParams();
+    const facultyId=params.fid; 
+    const [sname,setSname]=useState('');
+    const [syllabusCovered,setSyllabusCovered]=useState('');
+    const [actualSyllabus,setActualSyllabus]=useState('');
+    const [topicCovered,setTopicCovered]=useState('');
+    const [noc,setNoc]=useState('');
+    const [remarks,setRemarks]=useState('');
+    const [date,setDate]=useState('');
+    const [section,setSection]=useState('');
+    const [year,setYear]=useState('');
+   
+   
+    const PostSyllabus =async (e)=>{
+      console.log(facultyId);
+      e.preventDefault();
+      
+      const res=await fetch("/api/syllabus",{
+        method:"POST",
+        headers:{
+          "Content-type":"application/json"
+        },
+        body:JSON.stringify({
+          facultyId,sname,syllabusCovered,actualSyllabus,topicCovered,noc,remarks,date,section,year
+        })
+      });
+      const data=await res.json();
+      if(res.status===400){
+        window.alert(data.error);
+
+      }else{
+        window.alert("Syllabus status added successfully");
+        setSname('');
+        setSyllabusCovered('');
+        setActualSyllabus('');
+        setTopicCovered('');
+        setNoc('');
+        setRemarks('');
+        setDate('');
+        setYear('');
+        setSection('');
+      }      
+    }
+    let [subjectSelectList,setSubjectSelectList]=useState([]);
+
+    const fetchSubjectAllocationList=()=>{
+      console.log(facultyId);
+      fetch("/api/subjectAllocation/"+facultyId).then((res)=>res.json())
+      .then((data)=>{
+         data.map((item)=>(
+          setSubjectSelectList((subjectSelectList) => [...subjectSelectList,item.courseName])     
+         ))
+         console.log("Subject Allocation list"+subjectSelectList);
+      }) 
+   }
+
+   useEffect(() => {
+    fetchSubjectAllocationList();
+    setSubjectSelectList([]); 
+  },[])
+    
+    return (
+       <Modal
+       {...props}
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="contained-modal-title-vcenter">
+            Faculty Details
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <form method="POST"> 
+              <label className='col-sm-4'>Name of the Subject:</label>
+              <select className="col-sm-6" name="subjects" value={sname} onChange={ e => setSname(e.target.value)}>
+                    <option>Select Subject</option>
+                    {(subjectSelectList!=null)?
+                            subjectSelectList.map((item,index)=>(
+                              <option>{item}</option>
+                            )):""
+                    }
+              </select>   <br/>
+              <label className='col-sm-4'>Syllabus Covered:</label>
+              <input className='col-sm-6' type='textbox' value={syllabusCovered} onChange={ e => setSyllabusCovered(e.target.value)}/>
+              <label className='col-sm-4'>Actual Syllabus to be Covered:</label>
+              <input className='col-sm-6' type='textbox' value={actualSyllabus} onChange={ e => setActualSyllabus(e.target.value)}/>
+              <label className='col-sm-4' >Topic last covered:</label>
+              <input className='col-sm-6' type='textbox' value={topicCovered} onChange={ e => setTopicCovered(e.target.value)}/><br/>
+              <label className='col-sm-4' >No. of Classes in leading/lagging:</label>
+              <input className='col-sm-6' type='textbox' value={noc} onChange={ e => setNoc(e.target.value)}/><br/>
+              <label className='col-sm-4' >Remarks:</label>
+              <input className='col-sm-6' type='textbox' value={remarks} onChange={ e => setRemarks(e.target.value)}/><br/>
+              <label className='col-sm-4' >Date:</label>
+              <input className='col-sm-6' type='date' value={date} onChange={ e => setDate(e.target.value)}/><br/>
+              <label className='col-sm-4' >Section:</label>
+              <select className='col-sm-6' type='textbox' value={section} onChange={ e => setSection(e.target.value)}>
+                <option>Select Section</option>
+                <option>A</option>
+                <option>B</option>
+                <option>C</option>
+              </select><br/>
+              <label className='col-sm-4' >Year:</label>
+              <select className='col-sm-6' type='textbox' value={year} onChange={ e => setYear(e.target.value)}>
+              <option>Select Year</option>
+                <option>I</option>
+                <option>II</option>
+                <option>III</option>
+                <option>IV</option>
+              </select><br/>
+          </form>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant='primary' type='submit' style={{float:'right'}} onClick={PostSyllabus}>Submit</Button>
+        </Modal.Footer>
+      </Modal>
+
+    );
+  }
+  export default SyllabusAdditionModal;
---
+++ src/components/facultyComponents/SyllabusStatusComponent/SyllabusStatusHome.js
@@ -0,0 +1,93 @@
+import React,{useEffect,useState} from 'react';
+
+import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button';
+import {useParams} from "react-router-dom";
+
+import SyllabusAdditionModal from './SyllabusAdditionModal';
+
+function SyllabusStatusHome(props){
+   
+    const [modalShow, setModalShow] = useState(false);
+    let [SyllabusList,setSyllabusList]=useState(null);
+    const params=useParams();
+    const fid=params.fid; 
+    
+    const removeSyllabus=(id)=>{
+        fetch("/api/syllabus/"+id,{
+            method: 'DELETE',  
+        }).then((res)=>res.json())
+        .then((data)=>console.log(data))
+        alert("Removed Successfully");
+        fetchSyllabusList();
+    }
+
+    const fetchSyllabusList=()=>{
+        fetch("/api/syllabus/"+fid).then((res)=>res.json())
+        .then((data)=>{
+           setSyllabusList(data); 
+           console.log(data)    
+        }) 
+        setModalShow(false);
+    }
+    useEffect(() => {
+         fetchSyllabusList(); 
+    },[])
+   
+        return(
+            props.display?
+            <div className="container-fluid">
+            <h5  className="pt-5 pb-5">Syllabus Status</h5> 
+                
+                <Table responsive>
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Subject Name</th>
+                            <th>Syllabus Covered as on date(No. of Units Covered)</th>
+                            <th>Actual Syllabus to be Covered(As per lesson plan)</th>
+                            <th>Topic last covered</th>
+                            <th>No. of classes leading/lagging (+)/(-)</th>
+                            <th>Remarks(Reasons for not covering in time as per lesson plan)</th>
+                            <th>Date</th>
+                            <th>Section</th>
+                            <th>Year</th>
+                            <th>Remove syllabus status</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {(SyllabusList!=null)?
+                            SyllabusList.map((item,index)=>(
+                                <tr key={index}>
+                                    <td>{index+1}</td>
+                                    <td>{item.sname}</td>
+                                    <td>{item.syllabusCovered}</td>
+                                    <td>{item.actualSyllabus}</td>
+                                    <td>{item.topicCovered}</td>
+                                    <td>{item.noc}</td>
+                                    <td>{item.remarks}</td>
+                                    <td>{new Date(item.date).toDateString()}</td>
+                                    <td>{item.section}</td>
+                                    <td>{item.year}</td>
+                                    <td><Button variant="danger" onClick={()=>removeSyllabus(`${item._id}`)}>Remove</Button>{' '}</td>
+                                
+                                </tr>
+                            )):<tr></tr>
+                        }
+                    
+                    </tbody>
+                   
+                </Table>
+                <Button style={{float:'right'}} variant="primary" onClick={() => setModalShow(true)}>Add Syllabus Status</Button>
+
+                
+
+      <SyllabusAdditionModal
+        show={modalShow}
+        onHide={()=>fetchSyllabusList()}
+      />
+                
+            </div>:''
+        )
+}
+export default SyllabusStatusHome;
