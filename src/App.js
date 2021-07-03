ce0afc6e0cf65c6051c20fb57ecd7b6fe6e4e537
Rushya Harika Maddukuri <40599543+RushyaHarika@users.noreply.github.com>
2021-07-03 16:05:34 +0530

    updated app.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import React from "react"
-import Router from "./Router";
-
+import React,{useState} from "react"
+import {BrowserRouter,Router,Switch,Route} from 'react-router-dom';
+import LoginComponent from './components/LoginComponent';
+import HodHomeComponent from './components/hodComponents/HodHomeComponent';
+import FacultyHomeComponent from './components/facultyComponents/FacultyHomeComponent';
+import ForgotComponent from './components/ForgotComponent';
 
 function App() {
+    const [authenticated,setAuthenticated]=useState(false);
     return (
-      <Router />
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" exact component={() => <LoginComponent authenticated={authenticated} setAuthenticated={p => {setAuthenticated(p)}}/>}/>
+                <Route path="/hod" exact component={authenticated?HodHomeComponent:() => <LoginComponent authenticated={authenticated} setAuthenticated={p => {setAuthenticated(p)}}/>}/>
+                <Route path="/faculty/:id" exact component={authenticated?FacultyHomeComponent:() => <LoginComponent authenticated={authenticated} setAuthenticated={p => {setAuthenticated(p)}}/>}/>
+                <Route path="/forgot/:user" exact component={ForgotComponent}/>
+                <Route component={() => <LoginComponent authenticated={authenticated} setAuthenticated={p => {setAuthenticated(p)}}/>}/>
+            </Switch>   
+        
+        </BrowserRouter>
     )
 }
 
